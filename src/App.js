ac2ba7f3df986f3299bc8020220cb48772142bec
sweetyr928 <yeslyn0208@gmail.com>
2023-06-30 00:08:49 +0900

    feat : 메인 페이지 UI 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import styled from 'styled-components';
+import { useEffect } from 'react';
 import MemberHeader from './Components/memberHeader';
+import SideBar from './Components/Sidebar';
+import PostSection from './Components/postSection';
+import ChatSection from './Components/chatSection';
+
+const AppContainer = styled.div`
+  overflow: hidden;
+`;
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fcf9c6;
+  height: 100vh;
+`;
 
 function App() {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+  }, []);
   return (
-    <div>
-      <MemberHeader></MemberHeader>
-    </div>
+    <AppContainer>
+      <MemberHeader />
+      <MainContainer>
+        <SideBar />
+        <PostSection />
+        <ChatSection />
+      </MainContainer>
+    </AppContainer>
   );
 }
 
