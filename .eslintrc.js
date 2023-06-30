0e1abef05ab55e099f9d6a7730274d4fb89130a7
sweetyr928 <yeslyn0208@gmail.com>
2023-06-30 14:37:19 +0900

    eslint props 유효성 검사 off

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,37 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
 
   extends: [
     'airbnb-base',
     'eslint:recommended',
     'plugin:react/recommended',
-    'prettier',
+    'prettier'
   ],
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
       files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
         sourceType: 'script',
         requireConfigFile: false,
         ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
+          jsx: true
+        }
+      }
+    }
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     babelOptions: {
-      presets: ['@babel/preset-react'],
-    },
+      presets: ['@babel/preset-react']
+    }
   },
-  rules: { 'react/react-in-jsx-scope': 'off' },
-  parser: '@babel/eslint-parser',
+  rules: { 'react/react-in-jsx-scope': 'off', 'react/prop-types': 'off' },
+  parser: '@babel/eslint-parser'
 };
