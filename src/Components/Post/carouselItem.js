6f851a1766511912d4b1d3e4c61b05aba6d79881
sweetyr928 <yeslyn0208@gmail.com>
2023-07-03 11:17:43 +0900

    design : 캐러셀 아이템 width, height 속성 수정 및 호버 애니메이션 추가

--- src/Components/Post/carouselItem.js
+++ src/Components/Post/carouselItem.js
@@ -1,42 +1,95 @@
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 
 const ItemWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: flex-start;
-  margin: 10px 10px 10px 10px;
+  margin: 10px 10px 0px 10px;
   padding: 10px 10px 10px 10px;
   border-radius: 20px;
   background-color: #ffffff;
-  width: calc(95%);
-  height: calc(15%);
+  height: calc(13%);
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:hover {
+    transform: scale(1.01);
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  }
 `;
 
 const Title = styled.div`
   font-size: 18px;
   font-weight: bold;
+  transition: color 0.3s ease;
 `;
 
 const Content = styled.div`
   font-size: 16px;
   font-weight: 600;
-  /* margin: 5px 0px 0px 0px; */
+  transition: color 0.3s ease;
+  margin: 5px 0px 0px 0px;
 `;
 
 const Date = styled.span`
   font-size: 14px;
   font-weight: 300;
-  /* margin: 5px 0px 0px 0px; */
   align-self: flex-end;
 `;
 
-const CarouselItem = ({ title, content, date }) => (
-  <ItemWrapper>
-    <Title>{title}</Title>
-    <Content>{content}</Content>
-    <Date>{date}</Date>
-  </ItemWrapper>
-);
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const AnimatedCarouselItem = styled.div`
+  animation: ${fadeIn} 0.5s ease;
+`;
+
+const CarouselItem = ({ title, content, date }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <ItemWrapper
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      style={{
+        backgroundColor: isHovered ? '#f8f8f8' : '#ffffff'
+      }}
+    >
+      <AnimatedCarouselItem>
+        <Title
+          style={{
+            color: isHovered ? '#9eb23b' : '#3f3f3f'
+          }}
+        >
+          {title}
+        </Title>
+        <Content
+          style={{
+            color: isHovered ? '#555555' : '#3f3f3f'
+          }}
+        >
+          {content}
+        </Content>
+      </AnimatedCarouselItem>
+      <Date>{date}</Date>
+    </ItemWrapper>
+  );
+};
 
 export default CarouselItem;
