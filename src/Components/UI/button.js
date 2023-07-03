28ba77faf02a600a8154a0aeaca006c3c14b3e80
sweetyr928 <yeslyn0208@gmail.com>
2023-07-03 15:50:05 +0900

    feat : 공통 컴포넌트(초록 버튼, 하얀 버튼) 구현

---
+++ src/Components/UI/button.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const GreenButton = styled.button`
+  background-color: #c7d36f;
+  color: #ffffff;
+  cursor: pointer;
+  font-weight: 800;
+  border-radius: 20px;
+  font-size: 15px;
+  padding: 10px 10px 10px 10px;
+
+  &:hover {
+    background-color: #9eb23b;
+  }
+`;
+
+export const WhiteButton = styled.button`
+  background-color: #ffffff;
+  color: #3f3f3f;
+  cursor: pointer;
+  font-weight: 800;
+  border-radius: 20px;
+  font-size: 15px;
+  padding: 10px 10px 10px 10px;
+
+  /* &:hover {
+    background-color: #9eb23b;
+  } */
+`;
