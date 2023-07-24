2c6a4af35c60ae22c0807f2853fd98ff18ab236f
9lumint <9lumint@gmail.com>
2023-07-25 01:23:17 +0900

    feat : 요청 목록 DB 연결

--- src/Components/Chat/requestList.js
+++ src/Components/Chat/requestList.js
@@ -1,13 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import { useRecoilValue } from 'recoil';
 import { useQuery } from 'react-query';
+import { GiPlantSeed } from 'react-icons/gi';
+import { PiPlantDuotone } from 'react-icons/pi';
+import { BiSolidTree } from 'react-icons/bi';
+import { MdForest } from 'react-icons/md';
 import RequestListModal from '../UI/requestListModal';
 import { getReqestById } from '../API/Request/fetchRequest';
 import { userData } from '../../Recoil/atoms';
 import { GreenLoading } from '../UI/loading';
 
-const ReqListContainer = styled.section`
+const ReqListContainer = styled.article`
   display: flex;
   flex-direction: column;
   width: 100%;
@@ -17,11 +21,37 @@ const ReqListContainer = styled.section`
   background-color: #ffffff;
   padding: 10px 0px;
   section {
+    display: flex;
     width: 90%;
     background-color: #ffffff;
     margin: 6px;
     padding: 6px;
     border-bottom: 2px solid #ebebeb;
+    justify-content: space-around;
+  }
+  div {
+    display: flex;
+  }
+  .icon-container {
+    border: 2px solid #3f3f3f;
+    border-radius: 50%;
+    width: 30px;
+    heigth: 30px;
+    padding: 5px;
+    margin-right: 9px;
+  }
+  .message-container {
+    flex-direction: column;
+    width: 300px;
+    font-size: 14px;
+    .title {
+      margin-bottom: 8px;
+      font-size: 15px;
+      font-weight: 800;
+    }
+  }
+  .time-container {
+    font-size: 13px;
   }
 `;
 
@@ -37,6 +67,13 @@ const ModalBackground = styled.div`
   align-items: center;
 `;
 
+const profiles = [
+  <GiPlantSeed key={0} size="30" />,
+  <PiPlantDuotone key={1} size="30" />,
+  <BiSolidTree key={2} size="30" />,
+  <MdForest key={3} size="30" />
+];
+
 const RequestList = () => {
   const [isModalOpen, setModalOpen] = useState(false);
   const [selectedId, setSelectedId] = useState(0);
@@ -50,11 +87,19 @@ const RequestList = () => {
     return requestList;
   });
 
+  const handlerCloseModal = () => {
+    setModalOpen(false);
+  };
+
   return (
     <>
       {isModalOpen && (
         <>
-          <RequestListModal requestDetail={requestData[selectedId]} />
+          <RequestListModal
+            profiles={profiles}
+            requestDetail={requestData[selectedId]}
+            handlerCloseModal={handlerCloseModal}
+          />
           <ModalBackground
             onClick={() => {
               setModalOpen(false);
@@ -68,13 +113,31 @@ const RequestList = () => {
         ) : (
           requestData.map((data, idx) => (
             <section
+              className="item-container"
               key={idx}
               onClick={() => {
                 setSelectedId(idx);
                 setModalOpen(true);
               }}
             >
-              <p>{data.message}</p>
+              <div className="icon-container">
+                {profiles[data.helperLevel - 1]}
+              </div>
+              <div className="message-container">
+                <p className="title">{data.title}</p>
+                <p>
+                  {data.message.length > 12
+                    ? `${data.message.slice(0, 12)}…`
+                    : data.message}
+                </p>
+              </div>
+              <div className="time-container">
+                {`${new Date(
+                  data.createdAt.seconds * 1000
+                ).getHours()}:${new Date(
+                  data.createdAt.seconds * 1000
+                ).getMinutes()}`}
+              </div>
             </section>
           ))
         )}
